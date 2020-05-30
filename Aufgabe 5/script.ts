9d51e3445c99aa9ab25a762de7eb517ba24ce7d6
Cocoze <colleen.sarah.zeller@hs-furtwangen.de>
2020-05-30 13:04:15 +0200

    Aufgabe 5 fertig

--- Aufgabe 5/script.ts
+++ Aufgabe 5/script.ts
@@ -1,1 +1,87 @@
-console.log("Hallo");
+console.log("Europa");
+
+var emissionEuropa: number = 4209.3;
+var gesamtemissionEurpa: number = 100 / (31116.5 / 4209.3);
+var veränderungEuropa: number = (4209.3 - 4965.7) / 4965.7;
+var vergleichEuropa: number = 4209.3 - 4965.7;
+
+
+
+
+
+console.log("Die Emission von Europa ist: " + emissionEuropa + "kg CO2");
+console.log("Relativ zur Gesamtemission der Welt verursacht Europa damit " + gesamtemissionEurpa + "%");
+console.log("Für Europa hat sich 2018 im Vergleich zu 2008 die Emission um " + veränderungEuropa + "% verändert");
+console.log("2018 im Vergleich zu 2008 sind das " + vergleichEuropa + "kg CO2");
+
+
+console.log("Nordamerika");
+
+var emissionNordamerika: number = 6035.6;
+var gesamtemissionNordamerika: number = 100 / (31116.5 / 6035.6);
+var veränderungNordamerika: number = (6035.6 - 6600.4) / 6600.4;
+var vergleichNordamerika: number = 6035.6 - 6600.4;
+
+
+console.log("Die Emission von Nordamerika ist: " + emissionNordamerika + "kg CO2");
+console.log("Relativ zur Gesamtemission der Welt verursacht Nordamerika damit " + gesamtemissionNordamerika + "%");
+console.log("Für Nordamerika hat sich 2018 im Vergleich zu 2008 die Emission um " + veränderungNordamerika + "% verändert");
+console.log("2018 im Vergleich zu 2008 sind das " + vergleichNordamerika + "kg CO2");
+
+
+
+console.log("Südamerika");
+
+var emissionSüdamerika: number = 1261.5;
+var gesamtemissionSüdamerika: number = 100 / (31116.5 / 1261.5);
+var veränderungSüdamerika: number = (1261.5 - 1132.6) / 1132.6;
+var vergleichSüdamerika: number = 1261.5 - 1132.6;
+
+console.log("Die Emission von Nordamerika ist: " + emissionSüdamerika + "kg CO2");
+console.log("Relativ zur Gesamtemission der Welt verursacht Nordamerika damit " + gesamtemissionSüdamerika + "%");
+console.log("Für Nordamerika hat sich 2018 im Vergleich zu 2008 die Emission um " + veränderungSüdamerika + "% verändert");
+console.log("2018 im Vergleich zu 2008 sind das " + vergleichSüdamerika + "kg CO2");
+
+
+
+
+console.log("Afrika");
+
+var emissionAfrika: number = 1235.5;
+var gesamtemissionAfrika: number = 100 / (31116.5 / 1235.5);
+var veränderungAfrika: number = (1235.5 - 1028) / 1028;
+var vergleichAfrika: number = 1235.5 - 1028;
+
+
+console.log("Die Emission von Nordamerika ist: " + emissionAfrika + "kg CO2");
+console.log("Relativ zur Gesamtemission der Welt verursacht Nordamerika damit " + gesamtemissionAfrika + "%");
+console.log("Für Nordamerika hat sich 2018 im Vergleich zu 2008 die Emission um " + veränderungAfrika + "% verändert");
+console.log("2018 im Vergleich zu 2008 sind das " + vergleichAfrika + "kg CO2");
+
+
+
+console.log("Asien");
+
+var emissionAsien: number = 16274.1;
+var gesamtemissionAsien: number = 100 / (31116.5 / 16274.1);
+var veränderungAsien: number = (16274.1 - 12954.7) / 12954.7;
+var vergleichAsien: number = 16274.1 - 12954.7;
+
+console.log("Die Emission von Nordamerika ist: " + emissionAsien + "kg CO2");
+console.log("Relativ zur Gesamtemission der Welt verursacht Nordamerika damit " + gesamtemissionAsien + "%");
+console.log("Für Nordamerika hat sich 2018 im Vergleich zu 2008 die Emission um " + veränderungAsien + "% verändert");
+console.log("2018 im Vergleich zu 2008 sind das " + vergleichAsien + "kg CO2");
+
+
+
+console.log("Australien");
+
+var emissionAustralien: number = 2100.5;
+var gesamtemissionAustralien: number = 100 / (31116.5 / 2100.5);
+var veränderungAustralien: number = (2100.5 - 1993) / 1993;
+var vergleichAustralien: number = 2100.5 - 1993;
+
+console.log("Die Emission von Nordamerika ist: " + emissionAustralien + "kg CO2");
+console.log("Relativ zur Gesamtemission der Welt verursacht Nordamerika damit " + gesamtemissionAustralien + "%");
+console.log("Für Nordamerika hat sich 2018 im Vergleich zu 2008 die Emission um " + veränderungAustralien + "% verändert");
+console.log("2018 im Vergleich zu 2008 sind das " + vergleichAustralien + "kg CO2");
