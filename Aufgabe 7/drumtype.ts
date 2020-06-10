127d2b253b4514fc6d2766aba88b63a1d2cedd3b
Cocoze <colleen.sarah.zeller@hs-furtwangen.de>
2020-06-10 18:18:54 +0200

    buttons

--- Aufgabe 7/drumtype.ts
+++ Aufgabe 7/drumtype.ts
@@ -1,2 +1,12 @@
 var sound: HTMLAudioElement = new Audio("A.mp3");
 sound.play();
+
+window.addEventListener("load", function() {
+    var sound: HTMLAudioElement = new Audio("A.mp3");
+    sound.play();
+
+    function playsample(): void {
+document.querySelector(".play-button").setAttribute("style", "click");
+document.querySelector(".play-button").addEventListener("load", playsample);
+}
+};
